17ffcd7dd3a6afcc1257ab9b42a875dbc3b326a6
Andrew <andrew.vinocur@gmail.com>
2017-09-20 16:46:07 -0700

    fixed incorrectly named component

--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var ClockComponent = require('./TimeComponent');
+var TimeComponent = require('./TimeComponent');
 
 class ClockComponent extends React.Component {
   render() {
     return (
       <div>
-        <h1>Currently the time is</h1>;
-        <ClockComponent></ClockComponent>
+        <h1>Currently the time is</h1>
+        <TimeComponent></TimeComponent>
       </div>
-
-    )
-
+    );
   }
 }
 
