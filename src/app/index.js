ac312b1d09063d78a4fc6c15ee1cff83d809866d
Andrew <andrew.vinocur@gmail.com>
2017-09-19 17:23:42 -0700

    refactors code to use react suggested es6 syntax

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var ClockComponent = React.createClass({
-  render:function(){
-    return(
-      <h1>Heloooooooo Vinocur</h1>
-    );
+class ClockComponent extends React.Component {
+  render() {
+    return <h1>Heloooooooo me</h1>;
   }
-});
+}
 
 
 ReactDOM.render(<ClockComponent />, document.getElementById('clock'));
