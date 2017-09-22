93a5fd9a0e45151b31a2ebbba6950d3df8c03024
Andrew <andrew.vinocur@gmail.com>
2017-09-22 11:37:07 -0700

    follows order of date and time below it

--- src/app/index.js
+++ src/app/index.js
@@ -8,7 +8,7 @@ class ClockComponent extends React.Component {
   render() {
     return (
       <div>
-        <h1>Currently the time is</h1>
+        <h1>Current Date and Time</h1>
         <TimeComponent></TimeComponent>
       </div>
     );
